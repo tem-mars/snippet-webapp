c472a198bcb22290c6d1841de56308ef834bdd20
tem-mars <teerapatboriboon4202@gmail.com>
2024-09-15 12:16:28 +0700

    fix setup root url

--- main.go
+++ main.go
@@ -13,24 +13,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write([]byte("Hello from Snippetbox"))
- }
+}
 
- func snippetView(w http.ResponseWriter, r *http.Request)  {
+ func snippetView(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display specific snippet..."))
- }
- 
+}
+
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Create a new snippet..."))
 } 
 
-func main()  {
+func main() {
 	mux := http.NewServeMux() 
     mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-
+	
 	log.Println("Starting server on :4000") 
     err := http.ListenAndServe(":4000", mux) 
     log.Fatal(err)
-	
 }
