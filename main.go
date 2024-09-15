2b6cb30765e30cb1bebff403ccf044b4fa745905
tem-mars <teerapatboriboon4202@gmail.com>
2024-09-15 12:19:00 +0700

    fix -

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
- func snippetView(w http.ResponseWriter, r *http.Request) {
+func snippetView(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display specific snippet..."))
 }
 
@@ -25,11 +25,10 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	mux := http.NewServeMux() 
-    mux.HandleFunc("/", home)
+	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	
 	log.Println("Starting server on :4000") 
-    err := http.ListenAndServe(":4000", mux) 
-    log.Fatal(err)
+	err := http.ListenAndServe(":4000", mux) 
+	log.Fatal(err)
 }
